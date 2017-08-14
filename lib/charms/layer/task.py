1672b4a4c4d05ca2e6a5ef6cf0b3ddfd33f7ffdf
Feng Xia <feng_xia41103@hotmail.com>
2017-08-14 16:05:03 -0400

    Update task.py to return success code and stats from runner.run()

--- lib/charms/layer/task.py
+++ lib/charms/layer/task.py
@@ -196,16 +196,7 @@ class Runner(object):
             if t['unreachable'] > 0 or t['failures'] > 0:
                 run_success = False
 
-        # Dirty hack to send callback to save logs with data we want
-        # Note that function "record_logs" is one I created and put into
-        # the playbook callback file
-        # self.pbex._tqm.send_callback(
-        #     'record_logs',
-        #     user_id=self.extra_vars['user_id'],
-        #     success=run_success
-        # )
-
         # Remove created temporary files
         os.remove(self.hosts.name)
 
-        return stats
+        return run_success, stats

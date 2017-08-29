adc8039af2075770e9a4c278b70b4dbce6eb298a
Feng Xia <feng_xia41103@hotmail.com>
2017-08-29 12:11:00 -0400

    Clean up charms.ansible. I'm removing original method and its associated files. So this is the clean implementation completely originated from our learning.

--- Makefile
+++
@@ -1,7 +0,0 @@
-.PHONY: clean
-
-clean:
-	find . -name \*.pyc -delete
-	find . -name '__pycache__' -delete
-	rm -f .coverage
-
--- tests/test_ansible.py
+++
@@ -1,9 +0,0 @@
-from charms.ansible import Ansible
-from mock import patch
-import pytest
-
-
-class TestAnsible:
-
-    def test_something(self):
-        pass

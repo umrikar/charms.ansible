786fa3068488d35c8cf433662cedd0867cddbe35
umrikar <raghav40_it@yahoo.co.in>
2018-02-01 14:36:11 +0530

    Update task.py

--- lib/charms/layer/task.py
+++ lib/charms/layer/task.py
@@ -1,5 +1,4 @@
 import os
-import subprocess
 from tempfile import NamedTemporaryFile
 
 from ansible.executor import playbook_executor
@@ -111,9 +110,8 @@ class Runner(object):
                  private_key_file='',
                  become_pass='',
                  vault_pass='',
-                 verbosity=0,
-                 debug=False):
-        self.debug = debug
+                 verbosity=0):
+
         self.options = Options()
         self.options.tags = tags,
         self.options.private_key_file = private_key_file
@@ -173,7 +171,7 @@ class Runner(object):
 
         # os.environ['ANSIBLE_CONFIG'] = os.path.abspath(os.path.dirname(__file__))
 
-        
+        # pbs = ["%s/%s" % (pb_dir, pb) for pb in playbooks]
         pbs = [os.path.join(pb_dir, pb) for pb in playbooks]
 
         # Setup playbook executor, but don't run until run() called
@@ -185,48 +183,29 @@ class Runner(object):
             options=self.options,
             passwords=passwords)
 
-        # TODO: so here we construct a CLI line.
-        # For whatever reason, api is not taking account for `tags`!!
-        self.callme = [
-            'ansible-playbook',
-            '-i',
-            self.hosts.name,
-            ','.join(pbs),
-        ]
-        if tags:
-            self.callme += ['--tags', ','.join(tags)]
-        if extra_vars:
-            extra = ["%s=%s" % (k, v) for k, v in extra_vars.items()]
-            self.callme += ['--extra-vars', '"%s"' % (' '.join(extra))]
-        if self.options.module_path:
-            self.callme += ['--module-path',self.options.module_path]
-
     def run(self):
         # Results of PlaybookExecutor
-        if self.debug:
-            print "ansbile cmd: ", ' '.join(self.callme)
+        self.pbex.run()
+        stats = self.pbex._tqm._stats
 
-        # self.pbex.run()
+        # Test if success for record_logs
+        run_success = True
+        hosts = sorted(stats.processed.keys())
+        for h in hosts:
+            t = stats.summarize(h)
+            if t['unreachable'] > 0 or t['failures'] > 0:
+                run_success = False
 
-        # TODO: this is truly strange! I tried all subprocess calls,
-        # and this is the ONLY way that works! All others didn't, even though
-        # the printed cmd line is exactly the same, and can be executed
-        # if I copy & paste in a terminal. So strange!
-        return_code = subprocess.call(' '.join(self.callme), shell=True)
-        return True if return_code == 0 else False
+        # Dirty hack to send callback to save logs with data we want
+        # Note that function "record_logs" is one I created and put into
+        # the playbook callback file
+        # self.pbex._tqm.send_callback(
+        #     'record_logs',
+        #     user_id=self.extra_vars['user_id'],
+        #     success=run_success
+        # )
 
-        # self.pbex.run()
-        # stats = self.pbex._tqm._stats
+        # Remove created temporary files
+        os.remove(self.hosts.name)
 
-        # # Test if success for record_logs
-        # run_success = True
-        # hosts = sorted(stats.processed.keys())
-        # for h in hosts:
-        #     t = stats.summarize(h)
-        #     if t['unreachable'] > 0 or t['failures'] > 0:
-        #         run_success = False
-
-        # # Remove created temporary files
-        # os.remove(self.hosts.name)
-
-        # return run_success, stats
+        return stats

6c22f6210b2990514569a638dfee773b575f2dca
umrikar <raghav40_it@yahoo.co.in>
2018-02-17 23:09:29 +0530

    Update task.py

--- lib/charms/layer/task.py
+++ lib/charms/layer/task.py
@@ -26,7 +26,6 @@ class Runner(object):
         self.tags = tags
         self.extra_vars = extra_vars
         self.playbooks = playbooks
-        self.hostnames = hostnames
         if verbosity==0:
             self.verbosity=''
         else:
@@ -37,19 +36,6 @@ class Runner(object):
         # Become Pass Needed if not logging in as user root
         passwords = {'become_pass': self.become_pass}
 
-        # Parse hosts, I haven't found a good way to
-        # pass hosts in without using a parsed template :(
-        # (Maybe you know how?)
-        self.hosts = NamedTemporaryFile(delete=False, mode='wt')
-        self.hosts.write("""[run_hosts]\n%s""" % hostnames)
-        self.hosts.close()
-
-        # This was my attempt to pass in hosts directly.
-        #
-        # Also Note: In py2.7, "isinstance(foo, str)" is valid for
-        #            latin chars only. Luckily, hostnames are
-        #            ascii-only, which overlaps latin charset
-
         # Playbook to run. Assumes it is
         # local and relative to this python file
         # in "../../../playbooks" directory.
@@ -57,17 +43,10 @@ class Runner(object):
         pb_rel_dir = '../../../playbooks'
         playbook_path = os.path.join(dirname, pb_rel_dir)
         self.module_path = os.path.join(playbook_path, 'library')
-
-        # os.environ['ANSIBLE_CONFIG'] = os.path.abspath(os.path.dirname(__file__))
         pbs = [os.path.join(playbook_path, pb) for pb in self.playbooks]
-
-
-        # TODO: so here we construct a CLI line.
-        # For whatever reason, api is not taking account for `tags`!!
+        
         self.callme = [
             'ansible-playbook',
-            '-i',
-            self.hosts.name,
             ','.join(pbs),
             '-c',
             self.connection,
@@ -86,6 +65,5 @@ class Runner(object):
             print("ansbile cmd: ", ' '.join(self.callme))
 
         return_code = subprocess.call(' '.join(self.callme), shell=True)
-        os.remove(self.hosts.name)
         return True if return_code == 0 else False
 
